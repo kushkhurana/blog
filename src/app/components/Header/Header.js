ae1115f6df1a08dad11ca0b401e5bb1787e30ca9
kush khurana <kushkhurana0002@gmail.com>
2024-02-10 12:28:35 +0530

    adding mobile nav menu & its logic

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,16 +1,24 @@
 "use client"
 import Link from "next/link"
 import { usePathname } from "next/navigation";
+import { useRef, useState } from "react";
 import headerMenu from "./header-data"
 import "./header.css"
 const Header = () => {
   const headerData = headerMenu;
   const pathname = usePathname();
+  const navRef = useRef();
+  const [navOpen,setnavOpen] = useState(false);
+  const handleNavClick = (e) => {
+    setnavOpen(!navOpen);
+
+  }
   console.log(pathname);
   return (
-    <header className="header">
+    <header className={(navOpen) ? 'header open' : 'header'}>
         <h3>Next App</h3>
-        <ul>
+        <button className="mobile-navigation" onClick={(e) => {handleNavClick(e)}}><span></span></button>
+        <ul ref={navRef} style={(navOpen) ? {maxHeight:navRef.current.scrollHeight} : {}}>
           {
             headerData.map((item) => {
               return <li className={(pathname === item.path) ? 'active' : ''} key={item.label}>
